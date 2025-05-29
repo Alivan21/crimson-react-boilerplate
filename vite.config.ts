d53ccb2884d893a3becb7e036874a8aabcea88cb
Alivan21 <alfanalivan@gmail.com>
2025-05-30 00:53:55 +0700

    fix: react router dev tools error

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths(), reactRouter(), reactRouterDevTools()],
+  plugins: [tailwindcss(), tsconfigPaths(), reactRouterDevTools(), reactRouter()],
 });
